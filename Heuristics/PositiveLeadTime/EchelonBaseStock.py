9b35fc78b6b0c0989dbaec0aa4ffe6af5ed37bc3
BenSLowery <benjaminslowery@outlook.com>
2024-05-09 10:49:41 +0100

    new file:   Heuristics/PositiveLeadTime/CappedEchelonBaseStock.py 	new file:   Heuristics/PositiveLeadTime/EchelonBaseStock.py 	new file:   Heuristics/ZeroLeadTime/CentralControl.py 	new file:   Heuristics/ZeroLeadTime/LocalControl.py

---
+++ Heuristics/PositiveLeadTime/EchelonBaseStock.py
@@ -0,0 +1,5 @@
+# This will generate a set of feasible bounds which can be evaluated on a simulation to find the best
+
+def generate_echelon_base_stock_bounds():
+    # This will generate a set of feasible bounds which can be evaluated on a simulation to find the best
+    pass
---
+++ Heuristics/ZeroLeadTime/LocalControl.py
@@ -0,0 +1,8 @@
+# Uses backwards dynamic programming to calculate the optimal local control, then aggregates.
+
+class local_control_heuristic():
+    def __init__(self):
+        print('Instance Created')
+
+    def method():
+        print('Method Called')
